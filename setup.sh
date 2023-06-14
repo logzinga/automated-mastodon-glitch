4dcc4a2f29c5b25f3952d8d57425ea4f600fd583
logzinga <65839350+logzinga@users.noreply.github.com>
2023-06-14 10:49:09 +1000

    .gitignore addition

--- setup.sh
+++ setup.sh
@@ -3,7 +3,19 @@
 
 clear
 echo "Installing Dependencies"
-pacman -Syu ffmpeg imagemagick libidn libpqxx libxml2 libxslt libyaml nodejs postgresql redis ruby-bundler protobuf yarn zlib
+pacman -Syu ffmpeg imagemagick libidn libpqxx libxml2 libxslt libyaml nodejs postgresql redis ruby-bundler protobuf yarn zlib base-devel
 
 
+clear
+echo "Adding Mastodon User"
+useradd -m mastodon
+
+clear
+echo "Installing Ruby for user Mastodon"
+su - mastodon -c "git clone https://github.com/rbenv/rbenv.git ~/.rbenv"
+su - mastodon -c "cd ~/.rbenv && src/configure && make -C src"
+su - mastodon -c "echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc"
+su - mastodon -c "echo 'eval "$(rbenv init -)"' >> ~/.bashrc"
+su - mastodon -c "exec bash"
+su - mastodon -c "git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build"
 
