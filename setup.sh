d840929874c1ee66d6e962183a4acdf4a08502db
logzinga <65839350+logzinga@users.noreply.github.com>
2023-06-14 10:28:32 +1000

    installs dependancies

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# script written by logzinga, feel free to modify to fit your needs
+# this script is intended to be run with root
+
+clear
+echo "Installing Dependencies"
+pacman -Syu ffmpeg imagemagick libidn libpqxx libxml2 libxslt libyaml nodejs postgresql redis ruby-bundler protobuf yarn zlib
+
+
+
